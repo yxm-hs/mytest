5226605792a3f36124fdd46bbef3159e677cfec5
yxm <940889679@qq.com>
2020-07-14 11:45:45 +0800

    提交有User类

---
+++ Test/src/main/java/com/sram/bean/User.java
@@ -0,0 +1,11 @@
+package com.sram.bean;
+
+public class User {
+    private int id ;
+    private String password;
+    private String username;
+
+    public User(int id) {
+        this.id = id;
+    }
+}
